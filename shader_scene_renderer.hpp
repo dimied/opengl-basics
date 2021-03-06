bfd2e434922e375886cc26a8dc5e889659709880
dimied <dimidimidi@googlemail.com>
2021-03-06 22:00:35 +0100

    Ported to C++

--- shader_scene_renderer.hpp
+++ shader_scene_renderer.hpp
@@ -6,6 +6,10 @@
 #include "scene_renderer.h"
 #include "glsl.h"
 #include "glsl_macros.h"
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class ShaderSceneRenderer : public SceneRenderer
 {
